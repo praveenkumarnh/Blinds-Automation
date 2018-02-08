7883be328d5df64fa9dfa5f5bbdadaccc7458949
Jan Neduchal <honza.neduchal@gmail.com>
2018-02-08 12:43:14 +0100

    Generic update

--- mega/mega.ino
+++ mega/mega.ino
@@ -1,9 +1,38 @@
 void setup() {
-  // put your setup code here, to run once:
-
+// pin init
+//room1 = Oliver
+const int pin_room1_vblind1_up = 22;
+const int pin_room1_vblind1_down = 23;
+const int pin_room1_blind1_up = 24;
+const int pin_room1_blind1_down = 25;
+const int pin_room1_blind2_up = 26;
+const int pin_room1_blind2_down = 27;
+//room2 = Natalie
+const int pin_room2_vblind1_up = 28;
+const int pin_room2_vblind1_down = 29;
+const int pin_room2_blind1_up = 30;
+const int pin_room2_blind1_down = 31;
+//room3 = Corridor upstairs
+const int pin_room3_blind1_up = 31;
+const int pin_room3_blind1_down = 32;
+//room4 = Living room
+const int pin_room4_vblind1_up = 33;
+const int pin_room4_vblind1_down = 34;
+const int pin_room4_vblind2_up = 35;
+const int pin_room4_vblind2_down = 36;
+const int pin_room4_vblind3_up = 37;
+const int pin_room4_vblind3_down = 38;
+//room5 = Kitchen
+const int pin_room5_vblind1_up = 39;
+const int pin_room5_vblind1_down = 40;
+//room6 = Corridor downstairs
+const int pin_room6_vblind1_up = 41;
+const int pin_room6_vblind1_down = 42;
+//room7 = Parents room
+const int pin_room7_vblind1_up = 43;
+const int pin_room7_vblind1_down = 44;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
 }
